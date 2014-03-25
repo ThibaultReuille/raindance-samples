f0126b833dcfd675b7651f249bd3251ac3cc20d0
Thibault Reuille <thibault@opendns.com>
2014-03-24 19:41:02 -0700

    Added heightmap to charts example.

--- Charts.cc
+++ Charts.cc
@@ -2,6 +2,7 @@
 #include <raindance/Core/Camera/Camera.hh>
 #include <raindance/Core/Transformation.hh>
 #include <raindance/Core/Charts/LineChart.hh>
+#include <raindance/Core/Charts/HeightMap.hh>
 
 class Demo : public RainDance
 {
@@ -21,8 +22,11 @@ public:
 
     virtual void initialize()
     {
-        m_Camera.setOrthographicProjection(0, 1024, 0, 728, 0.1f, 1024.0f);
-        m_Camera.lookAt(glm::vec3(0.0, 0.0, 5.0), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+        m_Camera2D.setOrthographicProjection(0, 1024, 0, 728, 0.1f, 1024.0f);
+        m_Camera2D.lookAt(glm::vec3(0.0, 0.0, 5.0), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+
+        m_Camera3D.setPerspectiveProjection(60.0f, (float)m_Window->width() / (float)m_Window->height(), 0.1f, 1024.0f);
+        m_Camera3D.lookAt(glm::vec3(-50.0, 30.0, -50.0), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
 
         {
             m_LineChart1 = new LineChart(glm::vec2(0, 100), glm::vec2(0, 10));
@@ -41,24 +45,36 @@ public:
             m_LineChart1->update();
         }
         {
-            m_LineChart2 = new LineChart(glm::vec2(-50, 50), glm::vec2(0, 20));
+            m_LineChart2 = new LineChart(glm::vec2(-50, 50), glm::vec2(0, 10));
             m_LineChart2->setTitle("Gaussian Distributions");
             m_LineChart2->setBackgroundColor(glm::vec4(HEX_COLOR(0x222222), 1.0));
             m_LineChart2->setBorderColor(glm::vec4(HEX_COLOR(0x0D4F8B), 1.0));
 
             auto rand1 = m_LineChart2->addGraph("Gaussian 1", glm::vec4(HEX_COLOR(0xF7FF73), 1.0));
             for (float t = -50; t <= 50; t += 2.0)
-                rand1->addPoint(glm::vec2(t, 20 * gaussian(t / 10, 2.0, 1.0)));
+                rand1->addPoint(glm::vec2(t, 10 * gaussian(t / 10, 2.0, 1.0)));
 
             auto rand2 = m_LineChart2->addGraph("Gaussian 2", glm::vec4(HEX_COLOR(0xFF6159), 1.0));
             for (float t = -50; t <= 50; t += 2.0)
-                rand2->addPoint(glm::vec2(t, 20 * gaussian(t / 10, 0, 0.5)));
+                rand2->addPoint(glm::vec2(t, 10 * gaussian(t / 10, 0, 0.5)));
 
             auto rand3 = m_LineChart2->addGraph("Gaussian 3", glm::vec4(HEX_COLOR(0x73FFF6), 1.0));
             for (float t = -50; t <= 50; t += 2.0)
-                rand3->addPoint(glm::vec2(t, 20 * gaussian(t / 10, -2.0, 0.7)));
+                rand3->addPoint(glm::vec2(t, 10 * gaussian(t / 10, -2.0, 0.7)));
 
             m_LineChart2->update();
+        }
+        {
+            m_HeightMap = new HeightMap(50, 50);
+
+            for (unsigned int i = 0; i < 50; i++)
+                for (unsigned int j = 0; j < 50; j++)
+                {
+                    float x = 2 * M_PI * static_cast<float>(i) / 50.0 - M_PI;
+                    float y = 2 * M_PI * static_cast<float>(j) / 50.0 - M_PI;
+                    m_HeightMap->setValue(i, j, sin(x * x) + cos(y * y) + RANDOM_FLOAT(-0.25, 0.25));
+                }
+            m_HeightMap->update();
         }
 
         glClearColor(0.2, 0.2, 0.2, 1.0);
@@ -78,24 +94,44 @@ public:
         Transformation transformation;
 
         transformation.push();
+        transformation.translate(glm::vec3(-25, 15, -25));
+        m_HeightMap->draw(m_Context, transformation.state(), m_Camera3D.getViewMatrix(), m_Camera3D.getProjectionMatrix());
+        transformation.pop();
+
+        glClear(GL_DEPTH_BUFFER_BIT);
+
+        transformation.push();
         transformation.translate(glm::vec3(10, 10, 0.0));
         transformation.scale(glm::vec3(10, 10, 1));
-        m_LineChart1->draw(m_Context, transformation.state(), m_Camera.getViewMatrix(), m_Camera.getProjectionMatrix());
+        m_LineChart1->draw(m_Context, transformation.state(), m_Camera2D.getViewMatrix(), m_Camera2D.getProjectionMatrix());
         transformation.pop();
 
         transformation.push();
         transformation.translate(glm::vec3(10, 120, 0.0));
         transformation.scale(glm::vec3(10, 10, 1));
-        m_LineChart2->draw(m_Context, transformation.state(), m_Camera.getViewMatrix(), m_Camera.getProjectionMatrix());
+        m_LineChart2->draw(m_Context, transformation.state(), m_Camera2D.getViewMatrix(), m_Camera2D.getProjectionMatrix());
         transformation.pop();
 
         finish();
     }
 
+    virtual void idle()
+    {
+        float t = 0.1f * static_cast<float>(m_Clock.milliseconds()) / 1000.0f;
+        m_Camera3D.lookAt(glm::vec3(50 * cos(t), 25, 50 * sin(t)), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+
+        glutPostRedisplay();
+    }
+
 private:
-    Camera m_Camera;
+    Clock m_Clock;
+
+    Camera m_Camera2D;
+    Camera m_Camera3D;
+
     LineChart* m_LineChart1;
     LineChart* m_LineChart2;
+    HeightMap* m_HeightMap;
 };
 
 int main(int argc, char** argv)
